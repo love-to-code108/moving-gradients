837eef27339e6ada82fb97bb00a40f81fdb1300e
love-to-code108 <love.to.code108@gmail.com>
2025-02-03 13:44:59 +0530

    minor bug fix and adding the name

--- src/Pages/Animation.jsx
+++ src/Pages/Animation.jsx
@@ -7,14 +7,14 @@ export const Animation = () => {
 
   return (
     <div 
-    className=" relative"
-    style={{ overflow: "hidden" ,   width: "100%" , height: "100vh" , objectFit: "cover" , backgroundColor : "black"}}>
+    className=" relative flex "
+    style={{ overflow: "hidden" ,   width: "100%" , height: "100vh" , objectFit: "cover" , backgroundColor : "black" , zIndex: 10}}>
       
 
 
         {/* THE NOISE AND THE OTHER RADIAL GRADIENTS */}
-      <div className=" flex justify-center items-center">
-      <svg style={{ opacity:0.2 , }} viewBox="0 0 450 250" xmlns="http://www.w3.org/2000/svg">
+      <div style={{ zIndex: 100}} className=" flex justify-center items-center">
+      <svg className=" w-[100%] h-[200%]"   style={{ opacity:0.2 , zIndex:120}} viewBox="0 0 450 250" >
         <filter id="noiseFilter">
           <feTurbulence
             type="fractalNoise"
@@ -45,6 +45,16 @@ export const Animation = () => {
         {/* GRADIANT BALL 8 */}
         <div className=" gradiantBall8"></div>
 
+      </div>
+
+
+
+
+
+
+      {/* THE CENTER TEXT */}
+      <div style={{ zIndex:120}} className=" absolute text-white">
+          <h1>Hello World</h1>
       </div>
     </div>
   );
