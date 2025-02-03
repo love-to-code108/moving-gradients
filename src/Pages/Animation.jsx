4c8ae664b09293d901e9776056e0de3cd1dfc5f2
love-to-code108 <love.to.code108@gmail.com>
2025-02-03 13:49:14 +0530

    completed the animation and added the text

--- src/Pages/Animation.jsx
+++ src/Pages/Animation.jsx
@@ -53,8 +53,8 @@ export const Animation = () => {
 
 
       {/* THE CENTER TEXT */}
-      <div style={{ zIndex:120}} className=" absolute text-white">
-          <h1>Hello World</h1>
+      <div style={{ zIndex:120 , inset: "20rem"}} className=" absolute text-white text-7xl flex justify-center items-center">
+          <h1>This Was fun</h1>
       </div>
     </div>
   );
